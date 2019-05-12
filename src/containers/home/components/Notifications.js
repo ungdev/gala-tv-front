5a36fca25599fad12b6d4ace27ddd0d1356127b2
Arnaud Dufour <arnaud.dufour@utt.fr>
2019-05-13 01:01:02 +0200

    changed vertical notification image

--- src/containers/home/components/Notifications.js
+++ src/containers/home/components/Notifications.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { connect } from 'react-redux'
 import './styles/notifications.css'
 import img from '../assets/notif-tv.png'
+import imgv from '../assets/notif-tv-vertical.png'
 
 class Notifications extends React.Component {
   render() {
     if (!this.props.notification) return null
     return (
       <div className='notifications'>
-        <div className='notifications-container'>
-          <img src={img} alt='' />
+        <div className={window.innerWidth > window.innerHeight ?
+        'notifications-container notif-horizontal' :
+        'notifications-container notif-vertical'}>
+          <img src={window.innerWidth > window.innerHeight ? img : imgv} alt='' />
           <h1 className='notification-title'>
             {this.props.notification.title}
           </h1>

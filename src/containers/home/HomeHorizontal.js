f9ff7e4fb8e59b1846f570326eabe48443a6a276
Thomas de Lachaux <thomas.delachaux@gmail.com>
2022-05-11 12:04:29 +0200

    fix(ci): correct ununsed imports

--- src/containers/home/HomeHorizontal.js
+++ src/containers/home/HomeHorizontal.js
@@ -1,33 +1,31 @@
-import React from 'react'
-import './home.css'
-import Time from './components/Time'
-import Partners from './components/Partners'
-import Events from './components/Events'
-import Messages from './components/Messages'
-import Tweets from './components/Tweets'
-import logo from '../../assets/logo.png'
-import Notifications from './components/Notifications'
+import React from 'react';
+import './home.css';
+import Time from './components/Time';
+import Partners from './components/Partners';
+import Events from './components/Events';
+import logo from '../../assets/logo.png';
+import Notifications from './components/Notifications';
 
 class HomeHorizontal extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div className='home home-horizontal'>
-          <div className='home-left'>
-            <div className='home-logo'>
-              <img src={logo} alt='' />
+        <div className="home home-horizontal">
+          <div className="home-left">
+            <div className="home-logo">
+              <img src={logo} alt="" />
             </div>
             <Partners />
           </div>
-          <div className='home-middle'>
+          <div className="home-middle">
             <Time />
             <Events horizontal />
           </div>
         </div>
         <Notifications />
       </React.Fragment>
-    )
+    );
   }
 }
 
-export default HomeHorizontal
+export default HomeHorizontal;

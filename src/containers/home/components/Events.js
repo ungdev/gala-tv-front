5246670649728df0016771ab6dc4742af4aab9fe
Thomas de Lachaux <thomas.delachaux@gmail.com>
2022-05-11 13:07:21 +0200

    fix: correct design bugs

--- src/containers/home/components/Events.js
+++ src/containers/home/components/Events.js
@@ -1,44 +1,44 @@
-import React from 'react'
-import moment from 'moment'
-import { connect } from 'react-redux'
-import './styles/events.css'
-import EventHorizontal from './EventHorizontal'
-import EventVertical from './EventVertical'
+import React from 'react';
+import moment from 'moment';
+import { connect } from 'react-redux';
+import './styles/events.css';
+import EventHorizontal from './EventHorizontal';
+import EventVertical from './EventVertical';
 
 class Events extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      date: moment().format()
-    }
+      date: moment().format(),
+    };
   }
   componentDidMount() {
-    this.intervalID = setInterval(() => this.tick(), 1000)
+    this.intervalID = setInterval(() => this.tick(), 1000);
   }
   tick() {
     this.setState({
-      date: moment().format()
-    })
+      date: moment().format(),
+    });
   }
   componentWillUnmount() {
-    clearInterval(this.intervalID)
+    clearInterval(this.intervalID);
   }
   render() {
-    let { events } = this.props
+    let { events } = this.props;
     events = events
-      .filter(event => !moment().isAfter(event.end)) // remove finished events
+      .filter((event) => !moment().isAfter(event.end)) // remove finished events
       .sort((a, b) => {
         //sort by start date
-        if (moment(a.start).isAfter(b.start)) return 1
-        if (moment(a.start).isBefore(b.start)) return -1
-        return 0
+        if (moment(a.start).isAfter(b.start)) return 1;
+        if (moment(a.start).isBefore(b.start)) return -1;
+        return 0;
       })
-      .splice(0, 3)
+      .splice(0, 3);
     return (
-      <div className='home-events'>
+      <div className="home-events">
         <h1>Prochains événements</h1>
-        <div className='events-container'>
-          {events.map(event =>
+        <div className="events-container">
+          {events.map((event) =>
             this.props.horizontal ? (
               <EventHorizontal event={event} key={event.id} />
             ) : (
@@ -47,15 +47,15 @@ class Events extends React.Component {
           )}
         </div>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => ({
-  events: state.socketio.events
-})
+const mapStateToProps = (state) => ({
+  events: state.socketio.events,
+});
 
 export default connect(
   mapStateToProps,
   null
-)(Events)
+)(Events);
